30a6f654edec7dbe6d114144a967fe917be4b076
tarmo <trmjhnns@gmail.com>
2022-04-20 23:27:41 +0300

    Deploy script to scp to web; first steps in writing the code (jQuery trypouts).

--- src/scripts/h5p-degree-dictations.js
+++ src/scripts/h5p-degree-dictations.js
@@ -1,4 +1,8 @@
+const $ = H5P.jQuery; // this does not work...
+
+
 export default class DegreeDictations extends H5P.EventDispatcher {
+
   /**
    * @constructor
    *
@@ -8,7 +12,9 @@ export default class DegreeDictations extends H5P.EventDispatcher {
    */
   constructor(params, contentId, extras = {}) {
     super();
-    this.element = document.createElement('div');
+
+    // test if H5p and $ are available:
+    console.log("H5P, $", H5P, $);
 
     this.element = document.createElement('div');
     this.scale = params.scale;
@@ -16,6 +22,11 @@ export default class DegreeDictations extends H5P.EventDispatcher {
     this.degrees = params.degrees;
     this.element.innerText = "Correct scales are: " + this.degrees;
     console.log("Read from params: ", this.scale, this.level, this.degrees, params);
+    this.degreeInput = document.createElement('input'); //$("<input>", { id:"degreeInput", type:"text", value:"enter degrees" });
+    this.degreeInput.type = "text";
+    this.degreeInput.id = "degreeInput";
+
+    // UI: inputField, playButton, stopButton, respondeButton
 
     /**
      * Attach library to wrapper.
@@ -23,9 +34,33 @@ export default class DegreeDictations extends H5P.EventDispatcher {
      * @param {jQuery} $wrapper Content's container.
      */
     this.attach = function ($wrapper) {
-      $wrapper.get(0).classList.add('h5p-degree-dictations');
-      //this.element.innerHTML = "Correct scales are: " + this.degrees;
-      $wrapper.get(0).appendChild(this.element);
+
+      const self = this;
+      const container = $wrapper.get(0);
+      // container külge saab lisada (appendChild) DOM elemente, $wrapperi külge jQuery elemente.
+      console.log("Container:", container);
+      container.classList.add('h5p-degree-dictations');
+      container.appendChild(this.element);
+      const testDiv = document.createElement('div');
+      testDiv.innerHTML = '<br />Suvaline div<br/><b>>Kaherealine</b>';
+      container.appendChild(testDiv);
+
+      container.append(this.degreeInput);
+
+      const playButton = document.createElement('button');
+      playButton.innerText = "PLAY";
+      playButton.onclick = () => console.log("PLAY");
+      container.appendChild(playButton);
+
+      $wrapper.append($('<button>', {
+        text: "KONTROLLI",
+        id: 'checkButton',
+        click: function () {
+          console.log("Check", self.degreeInput.value);
+        }
+
+      }));
+
     };
   }
 }
