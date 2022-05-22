dd9623d1587db42512e4a781ee039c8e5d36e3c6
tarmo <trmjhnns@gmail.com>
2022-05-22 10:19:15 +0300

    Added version to getLibraryPath parameter. Version to 1.0.3

--- src/scripts/h5p-degree-dictations.js
+++ src/scripts/h5p-degree-dictations.js
@@ -95,9 +95,12 @@ export default class DegreeDictations extends H5P.EventDispatcher {
         //TODO: check if file exists
         sampleList[i]=i+".mp3";
       }
-      // seems I need to hardcode the path for now as H5P.getLibraryPath does not work correctly in development mode
-      const path = H5P.getLibraryPath("H5P.DegreeDictations") + "/dist/instruments/" + instrument + "/" ;//"/drupal7/sites/default/files/h5p/development/H5P.DegreeDictations/dist/instruments/" + instrument + "/";
-      console.log("Path: ", path, H5P.getLibraryPath("H5P.DegreeDictations"));
+      // seems that in development mode I need to hardcode the path for now as H5P.getLibraryPath does not work correctly in development mode
+      // const path = "/drupal7/sites/default/files/h5p/development/H5P.DegreeDictations/dist/instruments/" + instrument + "/";
+
+      //NB! parameter for getLibraryPath must include also the version like -1.0 UPDATE THIS LINE when semantiv version is changed!
+      const path = H5P.getLibraryPath("H5P.DegreeDictations-1.0") + "/dist/instruments/" + instrument + "/" ;
+      console.log("Path: ", path, H5P.getLibraryPath("H5P.DegreeDictations-1.0"));
       const sampler = new  Sampler( {
             urls: sampleList,
             baseUrl: path, //"./instruments/"+instrument + "/",
